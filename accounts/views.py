7cc6f73e7d70fd3bb87c0d25baa42bf70569aa38
9NickNack9 <niklas.kantele@hotmail.com>
2022-11-20 19:16:47 +0200

    Static files

--- accounts/views.py
+++ accounts/views.py
@@ -27,8 +27,8 @@ def registerPage(request):
             user = form.save()
             username = form.cleaned_data.get('username')
 
-            group = Group.objects.get(name='customer')
-            user.groups.add(group)
+            #group = Group.objects.get(name='customer')
+            #user.groups.add(group)
             Customer.objects.create(
                 user=user,
                 name=user.username,
